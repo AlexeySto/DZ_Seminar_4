3d9ea832931fbd0604e6dee407ce78b75f2f8db7
Алексей <stolyarov1986@inbox.ru>
2023-07-16 14:50:38 +0300

    added task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double Prompt(string message)                // Запрос ввода числа пользователем
+{
+    Console.Write(message);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double RaiseToDegree(double num, double deg)  // Возводим в степень
+{
+    return Math.Pow(num, deg);
+}
+
+double number = Prompt("Введите число: ");
+double degree = Prompt("Введите степень числа: ");
+
+Console.WriteLine($"{number}, {degree} -> {RaiseToDegree(number, degree)}");
